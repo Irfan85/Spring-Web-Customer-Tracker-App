493830b68c2a6a1e399b42a435ebe80c129cd6e9
Irfan85 <irfan.muhit85@gmail.com>
2022-04-14 19:44:49 +0600

    @GetMapping and @PostMapping annotations

--- src/main/java/com/example/springdemo/controller/CustomerController.java
+++ src/main/java/com/example/springdemo/controller/CustomerController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.springdemo.dao.CustomerDAO;
 import com.example.springdemo.entity.Customer;
@@ -18,7 +20,12 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")
+	// The "GetMapping" annotation will contrain this method to handle only get requests unlike
+	// the "RequestMapping" annotation which covers all HTTP methods. A similar "PostMapping"
+	// annotation is also available for only handling post methods. This type of contrain can also
+	// be applied in the "RequestMapping" method by the following code:
+	// @RequestMapping(path = "/list", method = RequestMethod.GET)
+	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		List<Customer> customers = customerDAO.getCustomers();
 		
